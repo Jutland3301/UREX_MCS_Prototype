030f2782c6752aad12dbcaf840ec283bb30e4430
Jutland3301 <ikatakozako@gmail.com>
2026-08-27 10:51:54 +0800

    Add TelemetrySimulator class for data generation

--- simulator/telemetry_simulator.py
+++ simulator/telemetry_simulator.py
@@ -1,1 +1,16 @@
+import random
 
+
+class TelemetrySimulator:
+    def __init__(self):
+        self.packet_count = 0
+
+    def generate(self):
+        self.packet_count += 1
+
+        return {
+            "temperature": round(random.uniform(20.0, 35.0), 2),
+            "voltage": round(random.uniform(4.8, 5.2), 2),
+            "rssi": random.randint(-90, -50),
+            "packet_count": self.packet_count,
+        }
